4b2ec4429b923eb70c22807defb48adc25e54a86
Rafi Siddique <rafisiddique67@gmail.com>
2025-01-15 19:46:25 +0600

    get my item router added

--- index.js
+++ index.js
@@ -18,13 +18,15 @@ app.use(cookieParser());
 
 const verifyToken = (req, res, next) => {
   const token = req?.cookies?.token;
-  if(!token) {
+  if (!token) {
     return res.status(401).send('Unauthorized Access');
   }
 
   jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
-    if(err) {
-      return res.status(401).send({ message: 'Unauthorized Access' })
+    if (err) {
+      return res.status(401).send({
+        message: 'Unauthorized Access'
+      })
     }
     req.user = decoded;
     next();
@@ -32,10 +34,14 @@ const verifyToken = (req, res, next) => {
 };
 
 app.get('/', async (req, res) => {
-    res.send('server is running');
+  res.send('server is running');
 });
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const {
+  MongoClient,
+  ServerApiVersion,
+  ObjectId
+} = require('mongodb');
 const uri = `mongodb+srv://${process.env.NAME}:${process.env.SECURITY_KEY}@cluster0.bk0nm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -52,28 +58,42 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    await client.db("admin").command({
+      ping: 1
+    });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     const db = client.db("WhereIsIt");
     const itemCollection = db.collection("item");
 
     app.get('/getAllItem', verifyToken, async (req, res) => {
-      const email = req.query.email;
-
-      if(req.user.email!==email) {
-        return res.status(403).send({ message: 'forbidden access' });
-      }
-
       const allItems = await (itemCollection.find()).toArray();
       res.send(allItems);
+    });
+
+    app.get('/getMyItem', verifyToken, async (req, res) => {
+      const email = req.user.user.email;
+
+      if (req.user.user.email !== email) {
+        return res.status(403).send({
+          message: 'forbidden access'
+        });
+      }
+
+      const query = {
+        email: email,
+      };
+      const result = await itemCollection.findOne(query);
+      res.send(result);
     });
 
     app.get('/getItem/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const email = req.query.email;
-      if(req.user.email!==email) {
-        return res.status(403).send({ message: 'forbidden access' });
+      if (req.user.user.email !== email) {
+        return res.status(403).send({
+          message: 'forbidden access'
+        });
       }
       const query = {
         _id: new ObjectId(id)
@@ -103,21 +123,27 @@ async function run() {
 
     app.post('/jwt', async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.TOKEN_SECRET, { expiresIn: '1h' });
+      const token = jwt.sign(user, process.env.TOKEN_SECRET, {
+        expiresIn: '1h'
+      });
       res.
       cookie('token', token, {
-        httpOnly: true,
-        secure: false,
-        sameSite: 'strict',
-      })
-      .send({ success: true });
+          httpOnly: true,
+          secure: false,
+          sameSite: 'strict',
+        })
+        .send({
+          success: true
+        });
     });
 
     app.put('/updateItems/:id', verifyToken, async (req, res) => {
       const email = req.query.email;
 
-      if(req.user.email!==email) {
-        return res.status(403).send({ message: 'forbidden access' });
+      if (req.user.user.email !== email) {
+        return res.status(403).send({
+          message: 'forbidden access'
+        });
       }
 
       const newItem = req.body;
@@ -146,11 +172,13 @@ async function run() {
       res.send(result);
     });
 
-    app.delete('/deleteItem/:id', verifyToken, async(req, res) => {
+    app.delete('/deleteItem/:id', verifyToken, async (req, res) => {
       const email = req.query.email;
 
-      if(req.user.email!==email) {
-        return res.status(403).send({ message: 'forbidden access' });
+      if (req.user.user.email !== email) {
+        return res.status(403).send({
+          message: 'forbidden access'
+        });
       }
 
       const id = req.params.id;
@@ -160,9 +188,9 @@ async function run() {
 
       const result = await itemCollection.deleteOne(query);
       res.send(result);
-    });    
+    });
 
-  } catch(err) {
+  } catch (err) {
     console.log(err.message);
     // Ensures that the client will close when you finish/error
     await client.close();
@@ -171,5 +199,5 @@ async function run() {
 run().catch(console.dir);
 
 app.listen(port, () => {
-    console.log(`server is running on port: ${port}`);
+  console.log(`server is running on port: ${port}`);
 });
